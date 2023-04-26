a96254193c73e5b37ce6468e04a77026cfa3375d
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-04-26 13:00:05 +0900

    commit

--- test/test_CSVPrinter.py
+++ test/test_CSVPrinter.py
@@ -19,14 +19,10 @@ class TestCSVPrinter(unittest.TestCase):
     def tearDown(self):
         print("Running tearDown")
     def test_read_1(self):
-        current_dir = os.getcwd()
-        print(current_dir)
-        print("Ruuning test_read_1")
         printer = CSVPrinter("./test/sample.csv")
         l = printer.read()
         self.assertEqual(3, len(l))
     def test_read_2(self):
-        print("Ruuning test_read_2")
         printer = CSVPrinter("./test/sample.csv")
         l = printer.read()
         self.assertEqual(3, len(l[0]))
